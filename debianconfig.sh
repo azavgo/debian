fa2b192d96a1afe21a102ea82c58433d79bf0e3f
azavgo <azavgo@yahoo.com>
2024-02-27 19:22:24 +1100

    Added supporting info for tastytrade

--- debianconfig.sh
+++ debianconfig.sh
@@ -119,4 +119,92 @@ gpg --output MY_FILENAME_secret.gpg --armor --export-secret-key GPG_PUB_KEY
 gpg --import MY_FILENAME_pub.gpg
 gpg --allow-secret-key-import --import MY_FILENAME_sec.gpg
 
+#Install Brave browser. Source https://brave.com/linux/
+sudo apt update 
+sudo apt upgrade 
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+#Install Yandex browser. Source: https://yandex.com/support/browser-beta/about/install.html
+sudo apt install software-properties-common
+sudo add-apt-repository "deb https://repo.yandex.ru/yandex-browser/deb beta main"
+curl https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG --output YANDEX-BROWSER-KEY.GPG
+#Reportedly this is depriciated. Needs to find out what is the modern way to add a key
+sudo apt-key add YANDEX-BROWSER-KEY.GPG
+#Open Firefox  https://browser.yandex.ru/beta/ and download and download Yandex.deb. The file will be dowloaded to ~/Downloads
+#Install gdebi
+sudo apt update 
+sudo apt install gdebi
+sudo gdebi ~/Downloads/Yandex.deb
+
+#Install Rust. Source: https://www.rust-lang.org/tools/install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#The output is the following: 
+#Welcome to Rust!
+
+#This will download and install the official compiler for the Rust
+#programming language, and its package manager, Cargo.
+
+#Rustup metadata and toolchains will be installed into the Rustup
+#home directory, located at:
+
+#  /home/azavgo/.rustup
+
+#This can be modified with the RUSTUP_HOME environment variable.
+
+#The Cargo home directory is located at:
+
+#  /home/azavgo/.cargo
+
+#This can be modified with the CARGO_HOME environment variable.
+
+#The cargo, rustc, rustup and other commands will be added to
+#Cargo's bin directory, located at:
+
+#  /home/azavgo/.cargo/bin
+
+#This path will then be added to your PATH environment variable by
+#modifying the profile files located at:
+
+#  /home/azavgo/.profile
+#  /home/azavgo/.bashrc
+
+#You can uninstall at any time with rustup self uninstall and
+#these changes will be reverted.
+
+#Current installation options:
+
+
+#   default host triple: x86_64-unknown-linux-gnu
+#     default toolchain: stable (default)
+#               profile: default
+#  modify PATH variable: yes
+
+#1) Proceed with installation (default)
+#2) Customize installation
+#3) Cancel installation
+
+
+#Install java to run tastytrade jre files
+sudo apt update
+sudo apt install default-jre
+
+#Install tastytrade platform. Source: https://tastytrade.com/trading-platforms/
+#Download tastytrade deb file
+
+#Create desktop-directories, this is simply to fix some old linux bug
+sudo mkdir /usr/share/desktop-directories
+
+#Now install tastytrade
+sudo gdebi ~/Downloads/tastytrade-2.9.0-1_amd64.deb
+#java -jar tastytrade-2.9.0.-1_amd64.jar
+
+#Add path to the tastytrade executable file for rofi app launcher
+#sudo vim ~/.bashrc
+#Add in the end of the file: export PATH="/opt/tastytrade/bin:$PATH"
+
+
 
